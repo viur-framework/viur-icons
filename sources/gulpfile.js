7d749ce4b44f4712f1678fe8084305d03e860e61
Sven Eberth <se@mausbrand.de>
2019-04-05 13:54:34 +0200

    improved gulp

--- sources/gulpfile.js
+++ sources/gulpfile.js
@@ -21,11 +21,12 @@ const path = require('path');
 const less = require('gulp-less');
 const postcss = require('gulp-postcss');
 const zindex = require('postcss-zindex');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const focus = require('postcss-focus');
 const nocomments = require('postcss-discard-comments');
 const nano = require('gulp-cssnano');
 const jmq = require('gulp-join-media-queries');
+const stylefmt = require('gulp-stylefmt');
 
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
@@ -48,21 +49,20 @@ gulp.task('css', () =>
 		.pipe(less({
 			paths: [path.join(__dirname, 'less', 'includes')]
 		}))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		})) // add vendor prefixes
 		.pipe(postcss([
+			autoprefixer({ // add vendor prefixes
+				browsers: ['last 2 versions'],
+				cascade: false
+			}),
 			nocomments, // discard comments
 			focus, // add focus to hover-states
 			zindex, // reduce z-index values
 		])) // clean up css
 		.pipe(jmq())
+		.pipe(stylefmt()) // syntax formatting
 		.pipe(gulp.dest(destpaths.css)) // save cleaned version
 		.pipe(nano()) // minify css
-		.pipe(rename(function (path) {
-			path.extname = '.min.css';
-		})) // save minified version
+		.pipe(rename({suffix: '.min'})) // save minified version
 		.pipe(gulp.dest(destpaths.css))
 );
 
